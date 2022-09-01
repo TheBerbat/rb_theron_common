7fb1dc9e63eb506e115c8939a82abd0ae3f0ddd0
Rafael Martin <rmartin@robotnik.es>
2022-09-01 11:09:06 +0200

    Extract funcionality docker image out

--- scripts/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Copy config files if exist
-#cp -f /config/robot.ros2_control.xacro /opt/ros/foxy/share/robotnik_base_hw/ros2_control/robot.ros2_control.xacro 2>/dev/null
-#cp -f /opt/ros/foxy/share/robotnik_base_hw/ros2_control/robot.ros2_control.xacro /config/robot.ros2_control.xacro 2>/dev/null
